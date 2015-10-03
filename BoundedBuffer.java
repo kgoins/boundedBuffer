718da0d0ef6f6079e5900ce440b76f4382cf6469
Kgoins <kgoins@Box0.local>
2015-10-03 16:13:09 -0400

    added BoundedBuffer

--- BoundedBuffer.java
+++ BoundedBuffer.java
@@ -1,4 +1,22 @@
 import java.util.ArrayList;
 public class BoundedBuffer<E> {
+	/* Stores the buffer itself and supports the plug in of any item type
+	as long as it has associated Producer / Consumer classes */
+
 	private ArrayList<E> buffer;
+	private int slots_full;
+
+	// Constructors
+	public BoundedBuffer<E>(int size) {
+		buffer = new ArrayList<E>(3);
+		for (int i = 0; i < 2; i++) {
+			buffer[i] = 0;
+		}
+	}
+
+	public void insert( E item, Producer p){
+		if ( slots_full == buffer.length() ) {
+		}
+		buffer.append(item);
+	}
 }
